7b3c896028bfacdf6e94a747e239f1e97b3f6572
Dirceu Belém <dirceubelem@gmail.com>
2024-03-12 14:00:34 -0300

    Enviando pra todo mundo

--- app/src/main/java/websocket/ChatServerEndpoint.java
+++ app/src/main/java/websocket/ChatServerEndpoint.java
@@ -4,13 +4,18 @@ import javax.websocket.*;
 import javax.websocket.CloseReason.CloseCodes;
 import javax.websocket.server.ServerEndpoint;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 @ServerEndpoint(value = "/app")
 public class ChatServerEndpoint {
 
+    public static Map<String, Session> sessions = new HashMap<>();
+
     @OnOpen
     public void onOpen(Session session) {
         System.out.println("Connected, sessionID = " + session.getId());
+        sessions.put(session.getId(), session);
         try {
             session.getBasicRemote().sendText("Seja bem vindo");
         } catch (IOException e) {
@@ -27,12 +32,21 @@ public class ChatServerEndpoint {
                 e.printStackTrace();
             }
         }
+        for (Map.Entry<String, Session> entry : sessions.entrySet()) {
+            Session s = entry.getValue();
+            try {
+                s.getBasicRemote().sendText(message);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
         System.out.println(session.getId() + " - " + message);
         return message;
     }
 
     @OnClose
     public void onClose(Session session, CloseReason closeReason) {
+        sessions.remove(session.getId());
         System.out.println("Session " + session.getId() + " closed because " + closeReason);
     }
 }
